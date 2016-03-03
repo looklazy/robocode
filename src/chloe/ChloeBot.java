88bd41201022d39d1108c8b735d80e6e902c76d6
Chloe(KimYoonSun) <daltago@coupang.com>
2016-03-03 12:23:32 +0900

    chloe bot

---
+++ src/chloe/ChloeBot.java
@@ -0,0 +1,38 @@
+package chloe;
+
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+import java.awt.*;
+
+public class ChloeBot extends Robot {
+
+	@Override
+	public void run() {
+		setColors(Color.pink, Color.yellow, Color.cyan, Color.white, Color.orange);
+
+		while (true) {
+			ahead(50);
+			turnLeft(10);
+			turnGunRight(360);
+		}
+	}
+
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(2);
+	}
+
+	@Override
+	public void onHitRobot(HitRobotEvent e) {
+		turnLeft(10 - e.getBearing());
+	}
+
+	@Override
+	public void onHitWall(HitWallEvent event) {
+		back(50);
+	}
+}
+
